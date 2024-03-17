5fc8abe7f23b75b65e4ff2ea999826774e4c55cc
marco <marco.paternosto@outlook.it>
2024-03-17 16:47:05 +0100

    first

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 sudo pacman -Syu
 yay -Syu
 
-sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd vim base-devel hyprland zsh brightnessctl alacritty neovim hyprpaper bluez blueman
+sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd vim base-devel hyprland zsh brightnessctl alacritty neovim hyprpaper bluez blueman nodejs
+
+sudo npm i -g @angular/cli
+sudo npm i -g sass
+sudo npm i -g nodemon
 
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
@@ -17,3 +21,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 cp .zshrc ~/
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+
+code --install-extension Mikael.Angular-BeastCode
+code --install-extension Angular.ng-template
+code --install-extension jdinhlife.gruvbox
+code --install-extension ecmel.vscode-html-css
+code --install-extension skyran.js-jsx-snippets
+code --install-extension PKief.material-icon-theme
+code --install-extension tomwhite007.rename-angular-component
+
