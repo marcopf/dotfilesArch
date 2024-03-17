05a4e57d12add1baf651086a13c9f3f32bb35f7f
marco <marco.paternosto@outlook.it>
2024-03-17 15:25:51 +0100

    first

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo pacman -Syu
+yay -Syu
+
+sudo pacman -S --noconfirm ttf-jetbrains-mono vim base-devel hyprland zsh brightnessctl alacritty neovim
+cp hyprland.conf ~/.config/hypr/
+cp ./.alacritty.toml ~/
+yay -S visual-studio-code-bin
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp .zshrc ~/
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
